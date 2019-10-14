d121ec5907744c5ed573a4f0d87ec86d0ba3dd1b
Chichri <35509775+Chichri@users.noreply.github.com>
2019-10-14 01:22:41 -0700

    Learing
    Apparetly I forgot to commit. Oh well

--- functions.py
+++ functions.py
@@ -28,11 +28,19 @@ greet_user('larry')
 print('\n')
 
 def describe_animal(animal_type, pet_name):
-    print('I have a pet ' + animal_type.title() + '.')
-    print('My ' + animal_type.title() + ' name is ' + pet_name.title() + '.' )
+    print('I have a pet ' + animal_type  + '.')
+    print('My ' + animal_type  + "'s name is " + pet_name.title() + '.' )
 
 describe_animal('hamster', 'harry')
 #This function uses positional arguements to handle more than one arguement
 #Like most things in python, posistional arguements are logically put in order
 #Simply call your parameters in the correct order separated by commas, and -
 #-everything works as normal.
+
+print('\n')
+
+describe_animal(animal_type='hamster', pet_name='harry')
+#This is a keyword arguement. The name and the value are directly associated
+#Keyword arguements do not have to be ordered correctly
+
+print('\n')
