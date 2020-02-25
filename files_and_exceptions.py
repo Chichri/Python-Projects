78239f6acd53f6d88af10140473d554473f30a5a
Chichri <farnezelameyer@gmail.com>
2020-02-25 15:31:58 -0800

    Learning
    Added slightly more, expanded on notes.

--- files_and_exceptions.py
+++ files_and_exceptions.py
@@ -8,3 +8,18 @@ with open('/Users/cole/Desktop/Coding_Projects/Python-Projects/pi_digits.txt') a
 #open() returns an object representing the file, which is stored in file_object
 #Once we have file_object, contents is assigned to the string in file_object -
 #- through the use of the read() method. Then contents is printed
+#I don't know why I had to list the path, only the file name should be required
+#open() is supposed to search the directory the program is in for the file
+#Keep in mind that diffrent machines/os's store files diffrently
+#Sometimes it's useful to store path's to variables for easy distinction
+
+print('\n')
+
+file_path = '/Users/cole/Desktop/Coding_Projects/Python-Projects/pi_digits.txt'
+with open(file_name) as file_path:
+    for line in file_object:
+        print(line)
+#Sometimes you'll want to check each line of whats written in a file
+#You might be looking for information or trying to modify the text
+#Either way, a simple for loop can cycle through an entire file instantly
+#(This is also an example of tying a filename/filepath to a vairable)
